9145edc4e5a4c07b4ce5447d935d36d9d7e74f49
Mahesh Nadar <mahesh.nadar@wohlig.com>
2017-09-21 14:07:23 +0530

    changes

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -111,112 +111,115 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         ];
 
     })
-    .controller('headerctrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal) {
+    .controller('headerctrl', function ($scope, $rootScope, TemplateService, NavigationService, $timeout, $uibModal) {
         $scope.template = TemplateService;
         $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
             $(window).scrollTop(0);
 
         });
 
+        $scope.HeaderAllDest = _.chunk(TemplateService.allplaces, 8);
+        var temp = _.filter(TemplateService.allplaces, ['popular', true]);
+        console.log(temp);
+        console.log(TemplateService.allplaces);
+        // $scope.HeaderAllDest = [
+        //     [{
+        //         _id: "amritsar_chandigarh",
+        //         name: "Amritsar Chandigarh",
+        //         image: "../img/destination/all/amritsar chandigarh/1.jpg"
+        //     }, {
+        //         _id: "andaman_nicobar",
+        //         name: "Andaman And Nicobar Island",
+        //         image: "../img/destination/all/andaman and nicobar island/1.jpg"
+        //     }, {
+        //         _id: "coorg",
+        //         name: "Coorg Kabini",
+        //         image: "../img/destination/all/coorg  kabini/1.jpg"
+        //     }, {
+        //         _id: "darjeeling",
+        //         name: "Darjeeling",
+        //         image: "../img/destination/all/darjeeling/1.jpg"
+        //     }, {
+        //         _id: "delhi_agra",
+        //         name: "Delhi Agra",
+        //         image: "../img/destination/all/delhi  agra/1.jpg"
+        //     }, {
+        //         _id: "goa",
+        //         name: "Goa",
+        //         image: "../img/destination/all/goa/1.jpg"
+        //     }],
+        //     [{
 
-        $scope.HeaderAllDest = [
-            [{
-                _id: "amritsar_chandigarh",
-                name: "Amritsar Chandigarh",
-                image: "../img/destination/all/amritsar chandigarh/1.jpg"
-            }, {
-                _id: "andaman_nicobar",
-                name: "Andaman And Nicobar Island",
-                image: "../img/destination/all/andaman and nicobar island/1.jpg"
-            }, {
-                _id: "coorg",
-                name: "Coorg Kabini",
-                image: "../img/destination/all/coorg  kabini/1.jpg"
-            }, {
-                _id: "darjeeling",
-                name: "Darjeeling",
-                image: "../img/destination/all/darjeeling/1.jpg"
-            }, {
-                _id: "delhi_agra",
-                name: "Delhi Agra",
-                image: "../img/destination/all/delhi  agra/1.jpg"
-            }, {
-                _id: "goa",
-                name: "Goa",
-                image: "../img/destination/all/goa/1.jpg"
-            }],
-            [{
+        //         _id: "jaipur",
+        //         name: "Jaipur",
+        //         image: "../img/destination/all/jaipur/1.jpg"
+        //     }, {
+        //         _id: "jaisalmer",
+        //         name: "Jaisalmer",
+        //         image: "../img/destination/all/jaisalmer/1.jpg"
+        //     }, {
+        //         _id: "jammu_kashmir",
+        //         name: "Jammu and Kashmir",
+        //         image: "../img/destination/all/jammu and Kashmir/1.jpg"
+        //     }, {
+        //         _id: "kovalam",
+        //         name: "Kovalam",
+        //         image: "../img/destination/all/kovalam/1.jpg"
+        //     }, {
+        //         _id: "kumarakom",
+        //         name: "Kumarakom",
+        //         image: "../img/destination/all/kumarakom/1.jpg"
+        //     }, {
+        //         _id: "leh_ladakh",
+        //         name: "Leh and Ladakh",
+        //         image: "../img/destination/all/leh and ladakh/1.jpg"
+        //     }],
+        //     [{
+        //         _id: "madurai",
+        //         name: "Madurai",
+        //         image: "../img/destination/all/madurai/1.jpg"
+        //     }, {
+        //         _id: "manali",
+        //         name: "Manali",
+        //         image: "../img/destination/all/manali/1.jpg"
+        //     }, {
+        //         _id: "munnar",
+        //         name: "Munnar",
+        //         image: "../img/destination/all/munnar/1.jpg"
+        //     }, {
+        //         _id: "mussourie_rishikesh",
+        //         name: "Mussourie  Rishikesh",
+        //         image: "../img/destination/all/mussourie  rishikesh/1.jpg"
+        //     }, {
+        //         _id: "pondicherry",
+        //         name: "Pondicherry",
+        //         image: "../img/destination/all/pondicherry/1.jpg"
+        //     }, {
+        //         _id: "shimla",
+        //         name: "Shimla",
+        //         image: "../img/destination/all/shimla/1.jpg"
+        //     }],
+        //     [{
+        //             _id: "sikkim_gangtok",
+        //             name: "Sikkim  Gangtok",
+        //             image: "../img/destination/all/sikkim  gangtok/1.jpg"
+        //         }, {
+        //             _id: "thekkady",
+        //             name: "Thekkady",
+        //             image: "../img/destination/all/thekkady/1.jpg"
+        //         }, {
+        //             _id: "udaipur",
+        //             name: "Udaipur",
+        //             image: "../img/destination/all/udaipur/1.jpg"
+        //         }, {
+        //             _id: "vythiri",
+        //             name: "Vythiri",
+        //             image: "../img/destination/all/vythiri/1.jpg"
+        //         }
 
-                _id: "jaipur",
-                name: "Jaipur",
-                image: "../img/destination/all/jaipur/1.jpg"
-            }, {
-                _id: "jaisalmer",
-                name: "Jaisalmer",
-                image: "../img/destination/all/jaisalmer/1.jpg"
-            }, {
-                _id: "jammu_kashmir",
-                name: "Jammu and Kashmir",
-                image: "../img/destination/all/jammu and Kashmir/1.jpg"
-            }, {
-                _id: "kovalam",
-                name: "Kovalam",
-                image: "../img/destination/all/kovalam/1.jpg"
-            }, {
-                _id: "kumarakom",
-                name: "Kumarakom",
-                image: "../img/destination/all/kumarakom/1.jpg"
-            }, {
-                _id: "leh_ladakh",
-                name: "Leh and Ladakh",
-                image: "../img/destination/all/leh and ladakh/1.jpg"
-            }],
-            [{
-                _id: "madurai",
-                name: "Madurai",
-                image: "../img/destination/all/madurai/1.jpg"
-            }, {
-                _id: "manali",
-                name: "Manali",
-                image: "../img/destination/all/manali/1.jpg"
-            }, {
-                _id: "munnar",
-                name: "Munnar",
-                image: "../img/destination/all/munnar/1.jpg"
-            }, {
-                _id: "mussourie_rishikesh",
-                name: "Mussourie  Rishikesh",
-                image: "../img/destination/all/mussourie  rishikesh/1.jpg"
-            }, {
-                _id: "pondicherry",
-                name: "Pondicherry",
-                image: "../img/destination/all/pondicherry/1.jpg"
-            }, {
-                _id: "shimla",
-                name: "Shimla",
-                image: "../img/destination/all/shimla/1.jpg"
-            }],
-            [{
-                    _id: "sikkim_gangtok",
-                    name: "Sikkim  Gangtok",
-                    image: "../img/destination/all/sikkim  gangtok/1.jpg"
-                }, {
-                    _id: "thekkady",
-                    name: "Thekkady",
-                    image: "../img/destination/all/thekkady/1.jpg"
-                }, {
-                    _id: "udaipur",
-                    name: "Udaipur",
-                    image: "../img/destination/all/udaipur/1.jpg"
-                }, {
-                    _id: "vythiri",
-                    name: "Vythiri",
-                    image: "../img/destination/all/vythiri/1.jpg"
-                }
+        //     ]
 
-            ]
-
-        ];
+        // ];
 
 
 
@@ -437,65 +440,128 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         $scope.navigation = NavigationService.getNavigation();
         console.log($stateParams);
         //default
-        $scope.getCustom = {
-            banner: "../img/banner.jpg",
-            images: [{
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                },
-                {
-                    image: "../img/manali.jpg",
-                    name: "Manali"
-                }
-            ],
-            accordion: [{
-                    icon: "../img/accordion/the_city.png",
-                    heading: "tab1",
-                    content: "description"
-                },
-                {
-                    icon: "../img/tbtlcons/the_city.png",
-                    heading: "tab2",
-                    content: "description"
-                },
-                {
-                    icon: "../img/tbtlcons/the_city.png",
-                    heading: "tab3",
-                    content: "description"
-                },
-                {
-                    icon: "../img/tbtlcons/the_city.png",
-                    heading: "tab4",
-                    content: "description"
-                }
-            ]
 
 
+        switch ($scope.currentStateId) {
+            case "amritsar_chandigarh":
+                //code
+                $scope.getCustom = {
+                    banner: "img/india/amritsar_chandigarh/banner.jpg",
+                    images: [{
+                            image: "img/india/amritsar_chandigarh/1.jpg",
+                            name: ""
+                        },
+                        {
+                            image: "img/india/amritsar_chandigarh/2.jpg",
+                            name: ""
+                        },
+                        {
+                            image: "img/india/amritsar_chandigarh/3.jpg",
+                            name: ""
+                        },
+                        {
+                            image: "img/india/amritsar_chandigarh/4.jpg",
+                            name: ""
+                        },
+                        {
+                            image: "img/india/amritsar_chandigarh/5.jpg",
+                            name: ""
+                        },
+                        {
+                            image: "img/india/amritsar_chandigarh/6.jpg",
+                            name: ""
+                        },
+                        {
+                            image: "img/india/amritsar_chandigarh/7.jpg",
+                            name: ""
+                        }
+                    ],
+                    accordion: [{
+                            icon: "../img/accordion/the_city.png",
+                            heading: "tab1",
+                            content: "description"
+                        },
+                        {
+                            icon: "../img/accordion/the_city.png",
+                            heading: "tab2",
+                            content: "description"
+                        },
+                        {
+                            icon: "../img/accordion/the_city.png",
+                            heading: "tab3",
+                            content: "description"
+                        },
+                        {
+                            icon: "../img/accordion/the_city.png",
+                            heading: "tab4",
+                            content: "description"
+                        }
+                    ]
+
+
+                }
+
+                break;
         }
+        // $scope.getCustom = {
+        //     banner: "img/banner.jpg",
+        //     images: [{
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         },
+        //         {
+        //             image: "../img/manali.jpg",
+        //             name: "Manali"
+        //         }
+        //     ],
+        //     accordion: [{
+        //             icon: "../img/accordion/the_city.png",
+        //             heading: "tab1",
+        //             content: "description"
+        //         },
+        //         {
+        //             icon: "../img/accordion/the_city.png",
+        //             heading: "tab2",
+        //             content: "description"
+        //         },
+        //         {
+        //             icon: "../img/accordion/the_city.png",
+        //             heading: "tab3",
+        //             content: "description"
+        //         },
+        //         {
+        //             icon: "../img/accordion/the_city.png",
+        //             heading: "tab4",
+        //             content: "description"
+        //         }
+        //     ]
+
+
+        // }
 
         $scope.accordian = [];
         $scope.accordian.push({
@@ -508,7 +574,7 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
     })
     .controller('ActivityCtrl', function ($scope, TemplateService, NavigationService, $timeout, toastr, $http) {
         $scope.template = TemplateService.getHTML("content/activity.html");
-        TemplateService.title = "Activity"; // This is the Title of the Website
+        TemplateService.title = "Activities"; // This is the Title of the Website
         $scope.navigation = NavigationService.getNavigation();
         $scope.allactiv = [{
                 _id: "amritsar",
--- frontend/js/services/template.js
+++ frontend/js/services/template.js
@@ -2,6 +2,112 @@ myApp.service('TemplateService', function () {
     this.title = "Home";
     this.meta = "";
     this.metadesc = "";
+    this.allplaces = [{
+            _id: "amritsar_chandigarh",
+            name: "Amritsar Chandigarh",
+            image: "../img/destination/all/amritsar chandigarh/1.jpg",
+            popular: true,
+
+        }, {
+            _id: "andaman_nicobar",
+            name: "Andaman And Nicobar Island",
+            image: "../img/destination/all/andaman and nicobar island/1.jpg",
+            popular: false
+        }, {
+            _id: "coorg",
+            name: "Coorg Kabini",
+            image: "../img/destination/all/coorg  kabini/1.jpg",
+            popular: false
+
+        }, {
+            _id: "darjeeling",
+            name: "Darjeeling",
+            image: "../img/destination/all/darjeeling/1.jpg",
+            popular: true
+        }, {
+            _id: "delhi_agra",
+            name: "Delhi Agra",
+            image: "../img/destination/all/delhi  agra/1.jpg"
+        }, {
+            _id: "goa",
+            name: "Goa",
+            image: "../img/destination/all/goa/1.jpg"
+        }, {
+            _id: "jaipur",
+            name: "Jaipur",
+            image: "../img/destination/all/jaipur/1.jpg"
+        }, {
+            _id: "jaisalmer",
+            name: "Jaisalmer",
+            image: "../img/destination/all/jaisalmer/1.jpg"
+        }, {
+            _id: "jammu_kashmir",
+            name: "Jammu and Kashmir",
+            image: "../img/destination/all/jammu and Kashmir/1.jpg"
+        }, {
+            _id: "kovalam",
+            name: "Kovalam",
+            image: "../img/destination/all/kovalam/1.jpg",
+            popular: true
+        }, {
+            _id: "kumarakom",
+            name: "Kumarakom",
+            image: "../img/destination/all/kumarakom/1.jpg"
+        }, {
+            _id: "leh_ladakh",
+            name: "Leh and Ladakh",
+            image: "../img/destination/all/leh and ladakh/1.jpg",
+            popular: true
+        }, {
+            _id: "madurai",
+            name: "Madurai",
+            image: "../img/destination/all/madurai/1.jpg"
+        }, {
+            _id: "manali",
+            name: "Manali",
+            image: "../img/destination/all/manali/1.jpg",
+            popular: true
+        }, {
+            _id: "munnar",
+            name: "Munnar",
+            image: "../img/destination/all/munnar/1.jpg"
+        }, {
+            _id: "mussourie_rishikesh",
+            name: "Mussourie  Rishikesh",
+            image: "../img/destination/all/mussourie  rishikesh/1.jpg"
+        }, {
+            _id: "pondicherry",
+            name: "Pondicherry",
+            image: "../img/destination/all/pondicherry/1.jpg"
+        }, {
+            _id: "shimla",
+            name: "Shimla",
+            image: "../img/destination/all/shimla/1.jpg"
+        }, {
+            _id: "sikkim_gangtok",
+            name: "Sikkim  Gangtok",
+            image: "../img/destination/all/sikkim  gangtok/1.jpg"
+        },
+        {
+            _id: "thekkady",
+            name: "Thekkady",
+            image: "../img/destination/all/thekkady/1.jpg"
+        },
+        {
+            _id: "udaipur",
+            name: "Udaipur",
+            image: "../img/destination/all/udaipur/1.jpg",
+            popular: true
+        },
+        {
+            _id: "vythiri",
+            name: "Vythiri",
+            image: "../img/destination/all/vythiri/1.jpg"
+        }
+
+    ];
+
+
 
     var d = new Date();
     this.year = d.getFullYear();
