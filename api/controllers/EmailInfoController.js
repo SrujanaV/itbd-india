2a21248d2a7fb6ff79a9d28038824a260b511c07
sailirawnang <avinashghare572@gmail.com>
2017-11-29 13:54:40 +0530

    done

--- api/controllers/EmailInfoController.js
+++ api/controllers/EmailInfoController.js
@@ -1,8 +1,3 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {
-    search1: function (req, res) {
-        console.log("inside search api EailInfo")
-        EmailInfo.search1(req.body, res.callback);
-    },
-};
+var controller = {};
 module.exports = _.assign(module.exports, controller);
