da6cce1b72829637fe3d2a98e16f0c2d7e532d48
sailirawnang <avinashghare572@gmail.com>
2017-11-29 16:22:27 +0530

    enquiry

--- frontend/js/controllers/footer-controller.js
+++ frontend/js/controllers/footer-controller.js
@@ -4,20 +4,48 @@ myApp.controller('footerCtrl', function ($scope, TemplateService, NavigationServ
         $(window).scrollTop(0);
     });
       $scope.showMsg=false
+      $scope.hideMsg=false
     console.log("inside footerctrl")
-$scope.submitEmail=function(data){
+// $scope.submitEmail=function(data){
 
-     NavigationService.saveMail(data, function (data1) {
-         console.log("data1",data1)
-         console.log("go");
-        //  $scope.data={};
-         if(data1.data.value==true){
-               $scope.showMsg=false
-               $scope.data={};
-         }else{
-             $scope.showMsg=true
-         }
-      });
-}
+//      NavigationService.saveMail(data, function (data1) {
+//          console.log("data1",data1)
+//          console.log("go");
+//         //  $scope.data={};
+//          if(data1.data.value==true){
+//                $scope.showMsg=false,
+//                $scope.hideMsg=true
+//                $scope.data={};
+//          }else{
+//              $scope.showMsg=true,
+//              $scope.hideMsg=false
+//          }
+//       });
+// }
+       $scope.subscribeSubmit = function(subscribeData) {
+            console.log("sadsadasdsads");
+            console.log("subscribeData", subscribeData);
+           NavigationService.saveMail(subscribeData, function (data) {
+                console.log("data", data.value);
+                if (data.data.data) {
+                    console.log('hhhhhhhhh');
+                    $scope.subscribeComplete = true;
+                    $timeout(function() {
+                        $scope.subscribeComplete = false;
+                        $scope.subscribeData = {};
+                    }, 2000);
+                } else {
+                    console.log('eeeeeeeeeee');
+                    console.log('inside elseeee');
+                    $scope.alreadySubscribed = true;
+                    $timeout(function() {
+                        $scope.alreadySubscribed = false;
+                        $scope.subscribeData = {};
+                    }, 2000);
+                }
+
+
+            })
+        }
 
 });
